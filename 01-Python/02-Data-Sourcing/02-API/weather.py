c71c38adc3b70827af3c4942c88bdd8e775dbc14
Nuria Aires <nuria.airesc@gmail.com>
2022-01-17 14:44:21 +0000

    initial folders

---
+++ 01-Python/02-Data-Sourcing/02-API/weather.py
@@ -0,0 +1,31 @@
+# pylint: disable=missing-module-docstring
+
+import sys
+import urllib.parse
+import requests
+
+BASE_URI = "https://www.metaweather.com"
+
+
+def search_city(query):
+    '''Look for a given city and disambiguate between several candidates. Return one city (or None)'''
+    pass  # YOUR CODE HERE
+
+def weather_forecast(woeid):
+    '''Return a 5-element list of weather forecast for a given woeid'''
+    pass  # YOUR CODE HERE
+
+def main():
+    '''Ask user for a city and display weather forecast'''
+    query = input("City?\n> ")
+    city = search_city(query)
+    # TODO: Display weather forecast for a given city
+    pass  # YOUR CODE HERE
+
+if __name__ == '__main__':
+    try:
+        while True:
+            main()
+    except KeyboardInterrupt:
+        print('\nGoodbye!')
+        sys.exit(0)
